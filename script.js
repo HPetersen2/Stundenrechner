80fe1bf15e649b255f4f8acb2eaa82cee72e3a49
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2024-09-19 18:40:53 +0200

    update

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 
-let year;
-
 function acceptTime() {
     let date = document.getElementById('workday-date').value;
     let startTime = document.getElementById('start-time').value;
     let endTime = document.getElementById('end-time').value;
-    for (let i = 6; i < 10; i++) {
-        year += date[i];
-    }
-    console.log(year);
-    let start = new Date (2024, );
+
+
 
 }
