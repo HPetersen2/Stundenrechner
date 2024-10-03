572dad6e1bad8d2e675277559151939581c43a07
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2024-10-03 07:41:36 +0200

    render time

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 
-function init() {
-    listEmployee();
-}
+/**
+ * This function load time from the userinput in a variable
+ * 
+ * @param {string} date - This is the date
+ * @param {string} startTime - This is the start time
+ * @param {string} endTime - This is the end time
+ */
 
 function acceptTime() {
     let date = document.getElementById('workday-date').value;
     let startTime = document.getElementById('start-time').value;
+    let startTimeString = `${date}T${startTime}:00`;
+    let start = new Date(startTimeString);
     let endTime = document.getElementById('end-time').value;
+    let endTimeString = `${date}T${endTime}:00`;
+    let end = new Date(endTimeString);
 
+    let workMinute = (end - start) / 1000 / 60;
+    let workHour = workMinute / 60;
 
-
+    calculatePayment(workHour);
 }
 
-// function listEmployee() {
-//     let employee = document.getElementById('employee');
-//     console.log(listEmployee);
-//     document.getElementById('employee-list').innerHTML = `Mitarbeiter: ${employee}`;
-// }
+function calculatePayment(workHour) {
+    let sum = workHour * 12.41;
+    let totalSum = sum.toFixed(2);
+    console.log(totalSum, "€");
+}
+
